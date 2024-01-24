a37b976336d1ae37fe7f5c0e83d060c7284e8f93
Michel Chowanski <michel@chowanski.de>
2024-01-24 23:50:37 +0100

    Add yaml schema validator

--- validate.js
+++ validate.js
@@ -1,19 +1,37 @@
 import { glob } from 'glob';
-import fs from 'fs';
-import YAML from 'yaml';
+import validateSchema from 'yaml-schema-validator';
 
 const files = await glob("./data/**/*.yaml");
 
-files.forEach((filePath) => {
-    const content = fs.readFileSync(filePath, 'utf8');
-
-    let data = null;
-
-    try {
-        data = YAML.parse(content);
-    } catch (error) {
-        throw new Error(`YAML file '${filePath}' is invalid. ${error}`);
+const monthSchema = [
+    {
+        days: { type: String, required: true },
+        from: { type: String, required: true },
+        to: { type: String, required: true },
+        event: { type: String, required: false },
+        season: { type: String, required: false },
     }
+];
 
+const schema = {
+    park: { type : String, required: true },
+    year: { type : Number, required: true },
+    months: {
+        january: monthSchema,
+        february: monthSchema,
+        march: monthSchema,
+        april: monthSchema,
+        may: monthSchema,
+        june: monthSchema,
+        july: monthSchema,
+        august: monthSchema,
+        september: monthSchema,
+        october: monthSchema,
+        november: monthSchema,
+        december: monthSchema,
+    }
+}
 
+files.forEach((filePath) => {
+    validateSchema(filePath, { schema: schema })
 })
